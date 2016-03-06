88fa0216102c08367080630c8a599c4f2dce7cfb
Thomas Guillerme <guillert@tcd.ie>
2016-03-06 15:23:50 +0000

    chapter3

---
+++ chap3.c
@@ -0,0 +1,24 @@
+/* Some comments on multiple lines
+   Like here */
+
+#include <stdio.h>
+
+int main (void)
+{
+    //Declare the variables
+    int value1, value2, sum;
+
+    //Excercise 1
+    printf("In C, lowervase letters are significant.\nmain is where program execution begins.\nOpening and cosing braces enclose program statements in a routine.\n All program statements must be terminated by a semicolon.\n");
+
+/*
+    //assign values
+    value1 = 50;
+    value2 = 25;
+    sum = value1 + value2;
+
+    //print sutff
+    printf ("The sum of %i and %i is %i\n", value1, value2, sum);
+*/
+    return 0;
+}
---
+++ prog2.c
@@ -0,0 +1,19 @@
+/* Some comments on multiple lines
+   Like here */
+
+#include <stdio.h>
+
+int main (void)
+{
+    //Declare the variables
+    int value1, value2, sum;
+
+    //assign values
+    value1 = 50;
+    value2 = 25;
+    sum = value1 + value2;
+
+    //print sutff
+    printf ("The sum of %i and %i is %i\n", value1, value2, sum);
+    return 0;
+}
