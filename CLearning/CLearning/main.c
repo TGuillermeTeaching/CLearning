dbbb3ecbe31779b310744d79370d9a72d954a267
Thomas Guillerme <guillert@tcd.ie>
2016-03-06 15:25:12 +0000

    Using Xcode for learning

---
+++ CLearning/CLearning/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CLearning
+//
+//  Created by Thomas on 06/03/2016.
+//  Copyright © 2016 Thomas. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
