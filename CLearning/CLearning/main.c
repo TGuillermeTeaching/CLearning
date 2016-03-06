ad24a0a13efe53d5abfd63f5d152b81039208a63
Thomas Guillerme <guillert@tcd.ie>
2016-03-06 16:16:57 +0000

    Chapter 4

--- CLearning/CLearning/main.c
+++ CLearning/CLearning/main.c
@@ -1,15 +1,13 @@
-//
-//  main.c
-//  CLearning
-//
-//  Created by Thomas on 06/03/2016.
-//  Copyright © 2016 Thomas. All rights reserved.
-//
+/* Some comments
+    on multiple lines*/
 
 #include <stdio.h>
+#include <math.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int main(void) {
+    
+    long double result = (3.31e-8L * 2.01e-7L) / (7.16e-6L+2.01e-8L);
+    printf("Results = %Lf\n", result);
+    
+    return 0 ;
 }
