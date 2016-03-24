4d6c4460a11e3ac18ff13c454dc3dc14639ad218
Thomas Guillerme <guillert@tcd.ie>
2016-03-24 12:04:35 +0000

    exercice 4

--- Pointers/11x.3.c
+++ Pointers/11x.3.c
@@ -10,14 +10,18 @@ struct entry
     struct entry *next;
 };
 
-
-// inserting entries in a list
-void insertEntry(struct entry *insert_position, struct entry *new_entry)
+// printing a list
+void printEntries(struct entry *list_start)
 {
-    new_entry->next = insert_position->next;
-    insert_position->next = new_entry;
+    // printing the new list
+    printf("Current list is:\n");
+    while ( list_start != (struct entry *) 0) {
+        printf("%i\n", list_start->value);
+        list_start = list_start->next;
+    }
 }
 
+// finding an entry in a list (and returning the pointer)
 struct entry *findEntry (struct entry *list_pointer, int match)
 {
     while ( list_pointer != (struct entry *) 0 )
@@ -28,11 +32,36 @@ struct entry *findEntry (struct entry *list_pointer, int match)
     return (struct entry *) 0;
 }
 
+
+// inserting entries in a list
+void insertEntry(struct entry *insert_position, struct entry *new_entry)
+{
+    // Get the new entry link to the next position (which is the one from the insert position entry)
+    new_entry->next = insert_position->next;
+    // Change the insert position pointer (next) to now point to the new entry
+    insert_position->next = new_entry;
+}
+
+// removing an entry in a list
+void removeEntry(struct entry *entry_to_remove)
+{   
+    // Change the pointer to the next element from the insert position to point to the next element
+    // i.e. change:
+    // previous_entry->next = entry_to_remove->next
+    entry_to_remove->next = entry_to_remove->next->next;
+}
+
 int main (void)
 {   
+    void printEntries(struct entry *list_start);
+    struct entry *findEntry (struct entry *list_pointer, int match);
     void insertEntry (struct entry *insert_position, struct entry *new_entry);
-    struct entry n1, n2, n3, n4, new_entry, *insert_position, *list_start, *first_entry;
-    int new_value, insertion_place;
+    void removeEntry(struct entry *entry_to_remove);
+
+    struct entry n1, n2, n3, n4, list_head, new_entry, *entry_to_remove, *insert_position, *list_start, *first_entry;
+    int new_value, insertion_place, value_remove;
+
+    list_head.next = &n1;
 
     n1.value = 100;
     n1.next = &n2;
@@ -50,11 +79,7 @@ int main (void)
     first_entry = &n1;
 
     // printing list
-    printf("Current list is:\n");
-    while ( list_start != (struct entry *) 0) {
-        printf("%i\n", list_start->value);
-        list_start = list_start->next;
-    }
+    printEntries(&n1);
 
     // Getting the new value
     printf("Which value to add? ");
@@ -64,19 +89,25 @@ int main (void)
     // Getting the insertion location pointer
     printf("After which value to add it? ");
     scanf("%i", &insertion_place);
-    list_start = &n1;
-    insert_position = findEntry(list_start, insertion_place);
+    insert_position = findEntry(&n1, insertion_place);
 
     //Inserting the value in the list
     insertEntry(insert_position, &new_entry);
 
-    // printing the new list
-    list_start = &n1;
-    printf("New list is:\n");
-    while ( list_start != (struct entry *) 0) {
-        printf("%i\n", list_start->value);
-        list_start = list_start->next;
-    }
+    // printing list
+    printEntries(&n1);
+
+    // Selecting a value to remove
+    printf("After which value to remove an entry? ");
+    scanf("%i", &value_remove);
+    entry_to_remove = findEntry(&n1, value_remove);
+
+    // Removing the entry
+    removeEntry(entry_to_remove);
+
+    // printing list
+    printEntries(&n1);
+
 
     return 0;
 }
